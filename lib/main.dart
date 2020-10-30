6c34f5c183994911a136c9e3d67dbecf6052c5d8
kelvin960606 <kelvin-wen@hotmail.com>
2020-10-30 11:08:31 +0800

    add easy loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 import 'package:stacked_services/stacked_services.dart';
 
@@ -31,6 +32,17 @@ class _MyAppState extends State<MyApp> {
       onGenerateRoute: AppRouter().onGenerateRoute,
       navigatorKey: locator<NavigationService>().navigatorKey,
       debugShowCheckedModeBanner: false,
+      builder: (BuildContext context, Widget child) {
+        // 确保 loading 组件能覆盖在其他组件之上.
+        // EasyLoading.show(status: 'loading...');
+        // EasyLoading.showProgress(1.0, status: 'downloading...');
+        // EasyLoading.showSuccess('Great Success!');
+        // EasyLoading.showError('Failed with Error');
+        // EasyLoading.showInfo('Useful Information.');
+        // EasyLoading.showToast('Toast');
+        // EasyLoading.dismiss();
+        return FlutterEasyLoading(child: child);
+      },
     );
   }
 }
